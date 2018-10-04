92bd1010a50dfce547f27b65a338d53b5a3e2a3d
locky <inox127@hotmail.com>
2018-10-04 04:06:20 +0200

    added exercice 3 files

---
+++ 03.Keyboard-Event/keyboard.js
@@ -0,0 +1,7 @@
+// Exercice 3 : keyboard event //
+
+// Exercice 3.1 : 
+    //   Crée (en HTML) une DIV avec l'id "character" //
+    //   Faire changer de couleur de fond a cette DIVà chaque fois qu'on appuie 
+    //   sur une touche numérique de 0 à 9 sur le clavier
+    //   Tip: utiliser le switch
